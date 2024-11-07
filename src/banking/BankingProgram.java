0cd5b550824691fc802915032ef334979b6bb49b
Arlene Ford <arlene.ford@seattlecolleges.edu>
2024-11-07 10:48:11 -0800

    updated lab-01, lab-02 and lab-03

---
+++ src/banking/BankingProgram.java
@@ -0,0 +1,29 @@
+package banking;
+
+import java.util.Scanner;
+
+public class BankingProgram {
+
+    public static void main(String[] args) {
+        //references are alan, barbara and carl
+        CheckingAccount alan    = new CheckingAccount("Alan", 1300);
+        CheckingAccount barbara = new CheckingAccount("Barbara", 850);
+        CheckingAccount carl    = new CheckingAccount("Carl", 500);
+
+        //FIXME: This is a security issue
+        System.out.println();
+        System.out.println();
+
+        System.out.println(alan);
+        System.out.println(barbara);
+        System.out.println(carl);
+
+        //TODO: Collect from keyboard
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter the name and balance: ");
+        String inputString = keyboard.nextLine();
+        CheckingAccount checkingAccount = new CheckingAccount(inputString);
+        System.out.println(checkingAccount);
+
+    }
+}
---
+++ src/banking/CheckingAccount.java
@@ -0,0 +1,53 @@
+package banking;
+
+public class CheckingAccount {
+    //data fields
+    String name;
+    double balance;
+
+    //constructor - this is the only way to make objects
+    public CheckingAccount(String input){
+        String[] parts = input.split(" ");
+        name = parts[0];
+        balance = Double.parseDouble(parts[1]);
+    }
+
+    public CheckingAccount(String name, double balance) {
+        this.name    = name;
+        this.balance = balance;
+    }
+
+    //methods - functions
+    public void deposit(double amount) {
+        balance = balance + amount;
+    }
+
+    public void withdraw(double amount) {
+        balance = balance - amount;
+    }
+
+    //accessors == "getters"
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    //mutators == "setters"
+    public void setBalance(double amount) {
+        balance = amount;
+
+    }
+
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    //create a toString method to display the information for the update
+    @Override
+    public String toString() {
+        return String.format("Name: %s\nBalance: %.2f\n", name, balance);
+    }
+}
